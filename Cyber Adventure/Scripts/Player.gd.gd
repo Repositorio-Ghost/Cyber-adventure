37b1ae0ccce11c43771303908cef55896f175f2a
Repositorio-Ghost <solido.gus@gmail.com>
2025-05-08 22:52:19 -0400

    Pre beta 0.2
    diálogos e interacciones con los npc listos

---
+++ Cyber Adventure/Scripts/Player.gd.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+@export var velocidad = 300.0
+
+var direccion = Vector2.ZERO
+@onready var animation_player = $AnimationPlayer
+
+func _physics_process(delta):
+	direccion = Input.get_vector("a", "d", "w", "s").normalized()
+	var movimiento = direccion * velocidad
+
+	if movimiento.length() > 0:
+		velocity = movimiento
+		if direccion.x > 0:
+			animation_player.play("caminar_derecha")
+		elif direccion.x < 0:
+			animation_player.play("caminar_izquierda")
+		elif direccion.y < 0:
+			animation_player.play("caminar_arriba")
+		elif direccion.y > 0:
+			animation_player.play("caminar_abajo")
+	else:
+		velocity = Vector2.ZERO
+		animation_player.play("idle")
+
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	if("npc_id" in body):
+		$"../dialodos".start(body.npc_id)
--- Cyber Adventure/Scripts/dialogos.gd
+++ Cyber Adventure/Scripts/dialogos.gd
@@ -1,44 +1,53 @@
 extends Control
- 
-const dialogs_npc = {
-	0:[
-		"Hola y Binvenido a Cyber Aventure, un juego educativo y emocionante donde aprenderas las bases de la informatica",
-	 	"yo soy el npc 0",
-		"¿y tu como te llamas?",
-		"...............",
-		"¡¡¡hola!!!",
-		"¿estas hay?",
-		"Que mal :,("
-		],
 
-	1:[
-		"quien eres",
-	 	"soy npc 1",
-	 	"no te conosco"
-		],
+const dialogs_npc = {
+	0: [
+		"Hola y Bienvenido a Cyber Adventure, un juego educativo y emocionante donde aprenderás las bases de la informática",
+		"Yo soy el NPC 0",
+		"¿Y tú cómo te llamas?",
+		"...............",
+		"¡¡¡Hola!!!",
+		"¿Estás ahí?",
+		"Qué mal :,("
+	],
+
+	1: [
+		"¿Quién eres?",
+		"Soy NPC 1",
+		"No te conozco"
+	],
 }
 
 var dialogs_index
-var  current_npc
+var current_npc
+var tween: Tween
 @onready var label = $RichTextLabel
 
 func _ready():
-	visible = false 
-	start(0)
+	visible = false
+	
 
 func start(npc_id):
 	visible = true
 	dialogs_index = 0
-	current_npc = npc_id 
+	current_npc = npc_id
 	speak()
 
-
 func speak():
+	label.visible_ratio = 0
 	label.text = dialogs_npc[current_npc][dialogs_index]
+	tween = create_tween()
+	var animation_speed = 0.05 * label.text.length()
+	tween.tween_property(label, "visible_ratio", 1, animation_speed)
 	dialogs_index += 1
 
 func next():
-	if(dialogs_index < dialogs_npc[current_npc].size()):
+	if tween.is_running():
+		tween.kill()
+		label.visible_ratio = 1
+		return
+
+	if dialogs_index < dialogs_npc[current_npc].size():
 		speak()
 	else:
 		visible = false
@@ -47,6 +56,6 @@ func _input(event):
 	if event.is_action_released("ui_accept"):
 		next()
 
-
-func _on_button_pressed():  
+func _on_button_pressed():
 	next()
+
